a5db0bc376cd1fdc45d443fe30929bfaa1ec4cc1
Joshua Mabotsa <Joshua.Mabotsa@standardbank.co.za>
2021-07-17 21:22:07 +0200

    Initial Commit

---
+++ DependencyContainer/DependencyContainer.h
@@ -0,0 +1,18 @@
+//
+//  DependencyContainer.h
+//  DependencyContainer
+//
+//  Created by Joshua Thotsana Mabotsa on 2021/07/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DependencyContainer.
+FOUNDATION_EXPORT double DependencyContainerVersionNumber;
+
+//! Project version string for DependencyContainer.
+FOUNDATION_EXPORT const unsigned char DependencyContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DependencyContainer/PublicHeader.h>
+
+
